556ae70d326ab514e4a19769ba2f1f33ede4d360
jack <1109546392@qq.com>
2026-01-16 20:49:37 +0800

    rm

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-数据模型
-"""
-
-from .edit_models import EditTaskStatus, EditRequest
-
-__all__ = ['EditTaskStatus', 'EditRequest']
--- backend/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""工具模块"""
-from .logger import setup_logger, TaskLogger, StageLogger
-from .storage import StorageManager, TaskDatabase
-
-__all__ = ['setup_logger', 'TaskLogger', 'StageLogger', 'StorageManager', 'TaskDatabase']
